10f48637b88b17f38be35548cf011a2ee28b9cbc
Gene De Lisa <gene@rockhoppertech.com>
2019-06-17 19:10:37 -0400

    updated readme

--- scripts/genXcode.sh
+++ scripts/genXcode.sh
@@ -3,4 +3,4 @@
 # Generate an Xcode project
 
 # -B <path-to-build> = Explicitly specify a build directory.
-cmake -G Xcode -H.. -B../buildMacOS
+cmake -G Xcode -H.. -B../buildMacOS -D USE_UNIT_TESTING=ON -D GENERATE_DOC=ON
